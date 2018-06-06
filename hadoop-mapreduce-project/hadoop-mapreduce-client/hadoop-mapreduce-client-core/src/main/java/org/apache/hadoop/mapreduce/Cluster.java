db96afd259c122681d432281fcc045a5be06acf4
martin-boettcher <martin.boettcher@brockmann-consult.de>
2018-06-06 21:50:06 +0200

    add missing single job status retrieval to job client

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/Cluster.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/Cluster.java
@@ -198,6 +198,10 @@ public class Cluster {
     }
     return null;
   }
+
+  public JobStatus getJobStatus(JobID jobId) throws IOException, InterruptedException {
+    return client.getJobStatus(jobId);
+  }
   
   /**
    * Get all the queues in cluster.
